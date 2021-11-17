c4661a384f0931ac76368f7da9c1a74d14b8ade1
meriambs <bensalah.meriem0@gmail.com>
2021-11-17 17:07:20 +0100

    test

---
+++ App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  <div style="border:solid 1px black;max-width:100vw" >
+
+<h1 class="title red">Your name here</h1>
+
+<br/>
+
+<img src="/imageInSrc.jpg" />
+
+<br/>
+
+<img src="/imageInPublic.jpg" /> 
+
+<div>
+
+<video width="320" height="240" controls>
+
+<source src="myVideo.mp4" type="video/mp4" />
+
+</video>
+    </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
